2161ca6fdc01214a72714a2a1b2d8367333a5f2c
Pongpanoth Panya <pongpanoth213@gmail.com>
2022-02-08 02:55:02 +0700

    fix missing import

--- midterm_front/src/routes/index.js
+++ midterm_front/src/routes/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../feature/home/page';
 import Login from '../feature/login/page'
 import { useController } from '../core/controller';
+import { useEffect } from 'react';
 
 export default function MainRoutes() {
     const globalController = useController()
